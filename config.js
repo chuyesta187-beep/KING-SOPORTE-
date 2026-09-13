a401d7b41c4d0e0e23cb4890ed5f95f7fa29414f
chuyesta187-beep <chuyesta187@gmail.com>
2026-09-13 06:47:33 -0500

    config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  guildId: process.env.GUILD_ID,
+
+  channels: {
+    ticketPanel: "1538681130293923896",
+    postulationPanel: "1538681130293923897",
+    ticketReviews: "1538681130293923898",
+    logs: "1538681131543695403"
+  },
+
+  roles: {
+    ticketStaff: "1541517711970934884",
+    postulationReview: "1538681128360214546"
+  }
+};
